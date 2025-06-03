cc4305b048a4df7c6d452c79defe89830ce6f650
Insoper <insoperpsttd@gmail.com>
2025-06-03 08:34:31 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,8 @@ self.addEventListener('install', function(e) {
       return cache.addAll([
         'index.html',
         'manifest.json',
-        'icons/icon-192.png',
-        'icons/icon-512.png'
+        'icon-192.png',
+        'icon-512.png'
       ]);
     })
   );
